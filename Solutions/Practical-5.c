0975846cef3675e95b5cb841a7e2a0e94140f727
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-06-29 22:03:21 +0530

    Create Practical-5.c

---
+++ Solutions/Practical-5.c
@@ -0,0 +1,24 @@
+/*
+Practical-5 Write a C program to enter a distance in to kilometre and convert it into meter, feet, inches and centimetres
+*/
+
+//Solutiion
+float meter,inch,cm,feet,km;
+printf("Enter the Distance in Kilometer: ");
+scanf("%f",&km);
+meter = 1000*km;
+cm = 100000*km;
+inch = 39370*km;
+feet = 3281*km;
+printf("\nDistance in KiloMeter: %f",km);
+printf("\nDistance in Meter: %f",meter);
+printf("\nDistance in Centimeter: %f",cm);
+printf("\nDistance in Inch: %f",inch);
+printf("\nDistance in Feet: %f",feet);
+
+
+/* Explantion
+
+This program converts given distance into KiloMeter, Meter, Centimeter, Inch, Feet.
+And prints all of them in different lines.
+*/
