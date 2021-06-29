003f3c39234a02b2b9d10a62697721f5b0db7a34
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-06-29 12:47:37 +0530

    Create Practical-1.c

---
+++ Solutions/Practical-1.c
@@ -0,0 +1,23 @@
+/*
+Practical-1 Write a program that performs as calculator (addition, multiplication, division, subtraction).
+*/
+
+// Solution
+
+int i, j , k; 
+i=10;
+j=5;
+k=i+j;
+printf(“Addition=%d”,k);
+k=i-j;
+printf(“Subtraction=%d”,k);
+k=i*j;
+printf(“Multiplication=%d”,k);
+k=i/j;
+printf(“Division=%d”,k);
+
+/*Explanation
+
+This is a simple program that acts as a calculator by doing Addition, Subtraction, Multiplication and Division using Arithmetic operators.
+
+*/
