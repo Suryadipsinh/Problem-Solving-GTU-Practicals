ab93028c6647b8f1fbb6b3579da738ce85a49634
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-06-30 23:56:46 +0530

    Create Practical-9.c

---
+++ Solutions/Practical-9.c
@@ -0,0 +1,24 @@
+/* 
+Practical-9 Write a program to read marks of a student from keyboard whether 
+the student is pass or fail (using if else)
+*/
+
+
+int marks;
+/* Code Area Begins */
+	printf("Enter Marks of the Student: ");
+	scanf("%d",&marks);
+	if(marks<35){
+	printf("Failed");
+            }
+else{
+            	printf("Passed");
+            }
+
+
+/* Explanation
+This program shows whether the student is pass or fail using if else statement.
+Again a simple C language program !
+*/
+
+
