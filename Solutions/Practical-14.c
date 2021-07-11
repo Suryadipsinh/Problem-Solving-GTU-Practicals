a012fb2fea31a448707997f504bcb24b9c8c0343
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-07-12 05:08:05 +0530

    Update Practical-14.c

--- Solutions/Practical-14.c
+++ Solutions/Practical-14.c
@@ -1,1 +1,32 @@
+/*
+Practical-14 Write a C program to read no 1 to 7 and print relatively day Sunday to Saturday.
+*/
 
+int i;
+/* Code Area Begins */
+	printf("Enter number between 1 to 7: ");
+	scanf("%d",&i);
+	switch(i) {
+	case 1: printf("Sunday");
+            		break;
+                        case 2: printf("Monday");
+                        	break;
+                        case 3: printf("Tuesday");
+                        	break;
+                        case 4: printf("Wednesday");
+                        	break;
+                        case 5: printf("Thursday");
+                        	break;
+                        case 6: printf("Friday");
+                        	break;
+                        case 7: printf("Saturday");
+                        	break;
+                        default: printf("Wrong Input");
+                        	break;
+            }
+
+/*
+Explanation
+This program uses switch cases to print week days and also uses 
+the ‘break’ to get out of switch case
+*/
