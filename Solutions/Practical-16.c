cffd13996d4a7c062095de6ed5f88c2b8fd54dcc
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-07-12 05:11:29 +0530

    Update Practical-16.c

--- Solutions/Practical-16.c
+++ Solutions/Practical-16.c
@@ -1,1 +1,23 @@
+/*
+Practical-16 Write a C program to input an integer number and check the last digit of number is even or odd.
+*/
+
+int no,ln;
+/* Code Area Begins */
+	printf("Enter the no");
+	scanf("%d",&no);
+           	ln = no%10;   //Last Digit of the number
+	if(ln % 2 == 0)
+	printf("Last Digit of %d is even",no);
+            else
+            	printf("Last Digit of %d is odd",no);
+
+/*
+Explanation:
+This program takes input as an integer number and check the last digit of 
+number is even or odd. To check the last-digit we divide that number by 2 and then 
+checking its reminder for the same. If reminder is 0 then its even number and else it’s odd number.
+*/
+
+
 
