954836f43de22969194b62aed206c1f72cf26b19
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-07-12 05:13:15 +0530

    Update Practical-17.c

--- Solutions/Practical-17.c
+++ Solutions/Practical-17.c
@@ -1,1 +1,29 @@
+/*
+Practical-17 Write a C program to find factorial of a given number.
+*/
+
+
+int fact(int n)
+{
+            	if(n==1)
+                            	return 1;
+            	else{
+                            	return(n*fact(n-1));
+            	}
+ 
+}
+void main()
+{
+            	int n;
+            	printf("Enter the no: ");
+            	scanf("%d",&n);
+            	printf("Factorial of %d is %d",n,fact(n));
+}
+
+/*
+Explanation
+This program uses the concept of recursion to find the factorial of the number. 
+When a function calls itself then it’s called recursion. In this program we can 
+see that fact() function calls itself inside it. This is how we find factorial of a number using recursion.
+*/
 
