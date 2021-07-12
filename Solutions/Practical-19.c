7a570deb2bb18e45d96cec03c90775473c8b4013
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-07-12 05:40:46 +0530

    Update Practical-19.c

--- Solutions/Practical-19.c
+++ Solutions/Practical-19.c
@@ -3,24 +3,31 @@ Practical-19 Write a program to generate first n number of Fibonacci series
 */
 
 
-int n,a=0,b=1,c,i=0;
-            	printf("Enter number: ");
-            	scanf("%d",&n);
-            	printf("\nFirst %d Fibonaaci Numbers are:\n",n);
-            	printf("%d,%d",a,b);
-            	while(i<n){
-                            	c=a+b;
-                            	printf("%d",c);
-                            	a=b;
-                            	b=c;
-                            	i++;
-            	}
+ivoid printFibonacciNumbers(int n)
+{
+    int f1 = 0, f2 = 1, i;
+ 
+    if (n < 1)
+        return;
+    printf("%d ", f1);
+    for (i = 1; i < n; i++) {
+        printf("%d ", f2);
+        int next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+    }
+}
+ 
+int main()
+{
+    printFibonacciNumbers(7);
+    return 0;
+}
 
 /*
 Explanation:
-
-
-I guess there is a problem in this program. Because it prints more than required numbers.
-I think we should use for loop instead while loop. This problem can be solved easily with for loop.
+This program prints n fibonacci numbers. it takes input as n and 
+then if n is less than one then it returns and for n>1 it will run
+for loop & we will assign f1 + f2 to next.
 */
 
